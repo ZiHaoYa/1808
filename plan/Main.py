d84d49065f63b014eeea188e55d66e707c1b181b
赵庆元 <qingyuan@geekniu.com>
2018-09-21 08:27:22 +0800

    code

--- plan/Main.py
+++ plan/Main.py
@@ -9,11 +9,16 @@ class PlaneGame(object):
         print("游戏初始化")
         self.screen = pygame.display.set_mode(SCREEN_RECT.size)  # 创建游戏窗口
         self.clock = pygame.time.Clock()  # 游戏时钟
-        self.__create_sprites()  # 创建精灵和精灵组
+        
 
         pygame.time.set_timer(CREATE_ENEMY_EVENT,1000)#定时器
+        pygame.time.set_timer(CREATE_BULLET_EVENT,100)#定时器1
 
         self.enemy_group = pygame.sprite.Group()
+
+        self.bullet_group = pygame.sprite.Group()
+
+        self.__create_sprites()  # 创建精灵和精灵组
 
     def start_game(self):
         print("开始游戏...")
@@ -31,7 +36,12 @@ class PlaneGame(object):
         bgs = BackGroundSprint()
         bgs1 = BackGroundSprint(False)#这个隐藏在屏幕上面
         self.bgsg = pygame.sprite.Group(bgs,bgs1)
-       
+
+
+        self.hero = HeroSprint()
+        self.hero_group = pygame.sprite.Group(self.hero)
+
+    
 
     def __event_handler(self):
         """事件监听"""
@@ -43,11 +53,56 @@ class PlaneGame(object):
             elif event.type == CREATE_ENEMY_EVENT:
                 enemy = EnemySprite()
                 self.enemy_group.add(enemy)
+            elif event.type == CREATE_BULLET_EVENT:
+                bullet = BulletSprite()
+                bullet.rect.x = self.hero.rect.centerx 
+                bullet.rect.y = self.hero.rect.top - 25
+
+                bullet1 = BulletSprite()
+                bullet1.rect.x = self.hero.rect.centerx -35
+                bullet1.rect.y = self.hero.rect.top +35
+
+                bullet2 = BulletSprite()
+                bullet2.rect.x = self.hero.rect.centerx +35
+                bullet2.rect.y = self.hero.rect.top +35
+
+
+                self.bullet_group.add(bullet,bullet1,bullet2)  
+
+        keys = pygame.key.get_pressed()
+        self.hero.speed1 = 0
+        self.hero.speed = 0
+        if keys[pygame.K_RIGHT]:
+            self.hero.speed = 4
+        elif keys[pygame.K_LEFT]:
+            self.hero.speed = -4
+        elif keys[pygame.K_DOWN]:
+            self.hero.speed1 = 4
+        elif keys[pygame.K_UP]:
+            self.hero.speed1 = -4
+        else:
+            self.hero.speed = 0 
+            self.hero.speed1 = 0      
+                
+
 
 
     def __check_collide(self):
         """碰撞检测"""
-        pass
+         # 1. 子弹摧毁敌机
+        pygame.sprite.groupcollide(self.bullet_group, self.enemy_group, True, True)
+
+        # 2. 敌机撞毁英雄
+        enemies = pygame.sprite.spritecollide(self.hero, self.enemy_group, True)
+
+        # 判断列表时候有内容
+        if len(enemies) > 0:
+
+        # 让英雄牺牲
+            self.hero.kill()
+
+                # 结束游戏
+            PlaneGame.__game_over()
 
 
     def __update_sprites(self):
@@ -57,6 +112,13 @@ class PlaneGame(object):
 
         self.enemy_group.update()
         self.enemy_group.draw(self.screen)
+
+
+        self.hero_group.update()
+        self.hero_group.draw(self.screen)
+
+        self.bullet_group.update()
+        self.bullet_group.draw(self.screen)
 
     @staticmethod
     def __game_over():
